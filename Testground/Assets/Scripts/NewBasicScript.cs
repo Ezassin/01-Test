36904053e319865d995be6c67131ca7a05bc9990
Ezassin <Dennis_Fleischerfleischer@web.de>
2025-04-12 02:10:26 +0200

    TestScript

---
+++ Testground/Assets/Scripts/NewBasicScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBasicScript : MonoBehaviour
+{
+        #region Variables / Properties    
+        
+        //Variables
+        
+        //Properties
+
+        #endregion
+                
+        #region Methods
+            
+             void FunctionName()
+             {
+             
+             }
+        #endregion  
+}
